e20b4d917cf6a166e5f5936f636e9eba1970e900
Leandro Lima <llimasilva@proton.me>
2025-01-16 21:30:53 -0300

    Class Line and line passing throug lines

--- Program.cs
+++ Program.cs
@@ -2,10 +2,21 @@
 using static Raylib_cs.Raylib;
 namespace HelloWorld;
 
+public class Line
+{
+  public int Height { get; set; }
+  public bool IsActive { get; set; } = false;
+  public Line(int height)
+  {
+    Height = height;
+  }
+}
 class Program
 {
-  const int MaxLines = 100;
+  public const int MaxLines = 100;
   public bool IsPaused = false;
+  public List<Line> Lines { get; set; } = new List<Line>();
+  private int CurrentLineIndex = 0;
   public void TogglePause()
   {
     IsPaused = !IsPaused;
@@ -13,40 +24,57 @@ class Program
   public static void Main()
   {
     Program program = new Program();
-
     Random random = new Random();
-    int[] numbers = new int[100];
 
-    // Atribuir valores usando um loop
-    for (int i = 0; i < numbers.Length; i++)
+    for (int i = 0; i < MaxLines; i++)
     {
-      numbers[i] = random.Next(1, 100);
+      program.Lines.Add(new Line(random.Next(1, 100)));
     }
     InitWindow(800, 480, "Algorithm Visualization");
-    SetTargetFPS(60);
+    SetTargetFPS(10);
     while (!WindowShouldClose())
     {
+      Console.WriteLine($"current index {program.CurrentLineIndex}");
+      Console.WriteLine($"program.IsPaused {program.IsPaused}");
       BeginDrawing();
       DrawText("Algorithm Visualization", 12, 12, 20, Color.Black);
       DrawText("Press SPACE to pause", GetScreenWidth() / 3, 12, 20, Color.Red);
-
+      ClearBackground(Color.White);
+      DrawFPS(GetScreenWidth() - 150, 12);
       if (IsKeyPressed(KeyboardKey.Space))
       {
         program.TogglePause();
       }
-      for (int i = 0; i < MaxLines; i++)
-      {
-        DrawLine(GetScreenWidth() / 5 + i, GetScreenHeight() - 10 - numbers[i], GetScreenWidth() / 5 + i, GetScreenHeight() - 10, Color.Blue);
-      }
-      ClearBackground(Color.White);
       if (program.IsPaused)
       {
         DrawText("Paused", 350, 200, 20, Color.Gray);
       }
-      DrawFPS(GetScreenWidth() - 150, 12);
+      else
+      {
+        if (program.CurrentLineIndex > 0)
+        {
+          program.Lines[program.CurrentLineIndex - 1].IsActive = false;
+        }
+        program.Lines[program.CurrentLineIndex].IsActive = true;
+        program.CurrentLineIndex = program.CurrentLineIndex + 1;
+        for (int i = 0; i < program.Lines.Count(); i++)
+        {
+          DrawLine(
+            GetScreenWidth() / 5 + i,
+            GetScreenHeight() - 10 - program.Lines[i].Height,
+            GetScreenWidth() / 5 + i, GetScreenHeight() - 10,
+            program.Lines[i].IsActive ? Color.Red : Color.Blue
+          );
+        }
+        if (program.CurrentLineIndex == MaxLines)
+        {
+          program.Lines.Last().IsActive = false;
+          program.CurrentLineIndex = 0;
+          program.IsPaused = true;
+        }
+      }
       EndDrawing();
     }
-
     CloseWindow();
   }
 }
